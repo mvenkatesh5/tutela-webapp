8f41245ce6247d924a660ff77b05fb5ad6f5bfbb
Guru Sainath <gurusainath007@gmail.com>
2021-06-22 06:38:03 +0530

    video infra updated

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,24 +1,23 @@
 import dynamic from "next/dynamic";
 import { SSRProvider, OverlayProvider } from "react-aria";
-
 import type { AppProps } from "next/app";
 import NProgress from "@components/nprogress";
 // import ResizeHandler from '@components/resize-handler';
-
 // axios config
 import "config/axios";
-
 // context provider
 import { GlobalContextProvider } from "@contexts/global";
 // components
 import ToastAlert from "@components/alert";
-
 const CrispWithNoSSR = dynamic(() => import("@constants/scripts/crisp"), { ssr: false });
 const SmartLookWithNoSSR = dynamic(() => import("@constants/scripts/smartLook"), { ssr: false });
-
 // styles
 import "@styles/app.scss";
 import "bootstrap/dist/css/bootstrap.min.css";
+// blueprint css
+import "@blueprintjs/core/lib/css/blueprint.css";
+import "@blueprintjs/icons/lib/css/blueprint-icons.css";
+import "@blueprintjs/datetime/lib/css/blueprint-datetime.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
--- pages/videos/index.tsx
+++ pages/videos/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 // next imports
 import Link from "next/link";
 // react bootstrap
-import { Container, Table, Form, Tab, Nav } from "react-bootstrap";
+import { Container, Table, Badge } from "react-bootstrap";
 // swr
 import useSWR, { mutate } from "swr";
 // blueprint date range
@@ -11,11 +11,13 @@ import { DateRangeInput } from "@blueprintjs/datetime";
 // layouts
 import StudentLayout from "@layouts/studentLayout";
 // global imports
-import { datePreview } from "@constants/global";
+import { datePreview, returnDate, dateTimeFormat } from "@constants/global";
 // api routes
-import { USER_ENDPOINT, USER_MESSAGE_ENDPOINT } from "@constants/routes";
+import { USER_CALENDAR_SESSION_ENDPOINT } from "@constants/routes";
 // api services
-import { APIFetcher, APIUpdater } from "@lib/services";
+import { APIFetcher } from "@lib/services";
+// cookie
+import { getAuthenticationToken } from "@lib/cookie";
 // hoc
 import withGlobalAuth from "@lib/hoc/withGlobalAuth";
 // global context provider
@@ -24,6 +26,22 @@ import { globalContext } from "@contexts/global";
 const VideoView = () => {
   const [globalState, globalDispatch] = React.useContext(globalContext);
 
+  const [userRole, setUserRole] = React.useState<any>();
+  const [tokenDetails, setTokenDetails] = React.useState<any>();
+  React.useEffect(() => {
+    if (getAuthenticationToken()) {
+      let details: any = getAuthenticationToken();
+      details = details ? JSON.parse(details) : null;
+      if (details) {
+        setTokenDetails(details);
+        if (details.info.role === 2) setUserRole("admin");
+        else if (details.info.role === 1) setUserRole("teacher");
+        else setUserRole("student");
+      }
+    }
+  }, []);
+
+  const [currentDateQuery, setCurrentDateQuery] = React.useState<any>();
   const [startDate, setStartDate] = React.useState<any>();
   const [endDate, setEndDate] = React.useState<any>();
   const handleDateChange = (value: any) => {
@@ -31,25 +49,51 @@ const VideoView = () => {
     setEndDate(value[1]);
   };
 
-  // const { data: userList, error: userListError } = useSWR(USER_ENDPOINT, APIFetcher, {
-  //   refreshInterval: 0,
-  // });
+  React.useEffect(() => {
+    if (startDate && endDate) {
+      let currentRoute = `start_date=${returnDate(startDate)}&end_date=${returnDate(endDate)}`;
+      if (userRole && userRole != "admin") {
+        if (tokenDetails && tokenDetails.user && tokenDetails.user.id) {
+          currentRoute = currentRoute + `&user_id=${tokenDetails.user.id}`;
+        }
+      }
+      setCurrentDateQuery(currentRoute);
+    }
+  }, [startDate, endDate]);
 
-  // const { data: messages, error: messagesError } = useSWR(
-  //   userList ? `${USER_MESSAGE_ENDPOINT}?is_actionable=true` : null,
-  //   (url) => APIFetcher(url),
-  //   {
-  //     refreshInterval: 0,
-  //   }
-  // );
+  const disablePreviousDate = (date: any) => {
+    var currentDate = new Date();
+    var endDate = new Date(date);
+    if (endDate >= currentDate) {
+      return true;
+    } else {
+      return false;
+    }
+  };
 
-  // if (messagesError) console.log(messagesError);
+  const { data: sessionList, error: sessionListError } = useSWR(
+    currentDateQuery && currentDateQuery
+      ? [
+          USER_CALENDAR_SESSION_ENDPOINT(currentDateQuery && currentDateQuery),
+          currentDateQuery && currentDateQuery,
+        ]
+      : null,
+    (url) => APIFetcher(url),
+    { refreshInterval: 5000 }
+  );
+
+  if (sessionListError) console.log(sessionListError);
+
+  console.log(sessionList);
 
   return (
     <div>
       <StudentLayout>
-        <Container>
-          <h5>My Videos</h5>
+        <Container className="pt-3 pb-3">
+          <h5 className="mb-3">My Videos</h5>
+          <div>
+            <small className="text-secondary">Select Date Range</small>
+          </div>
           <DateRangeInput
             formatDate={(date) => date.toLocaleString()}
             onChange={handleDateChange}
@@ -59,11 +103,72 @@ const VideoView = () => {
             value={[startDate, endDate]}
           />
         </Container>
-        {/* {!messages ? (
-            <div className="text-center mt- 5 mb-5">Loading.....</div>
-          ) : (
-            <Container></Container>
-          )} */}
+        {startDate && endDate ? (
+          <>
+            {!sessionList && !sessionListError ? (
+              <div className="text-center tex-secondary mt- 5 mb-5">Loading.....</div>
+            ) : (
+              <Container>
+                <Table className="table table-bordered">
+                  <thead>
+                    <tr>
+                      <th>S.No</th>
+                      <th>
+                        <div>Title</div>
+                        <div>Description</div>
+                      </th>
+                      <th>
+                        <div>Start Date</div>
+                        <div>End Date</div>
+                      </th>
+                      <th>Status</th>
+                      <th>Link</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {sessionList &&
+                      sessionList.length > 0 &&
+                      sessionList.map((session: any, index: any) => (
+                        <tr key={`session-table-${index}`}>
+                          <td>{index + 1}</td>
+                          <td>
+                            <div>{session.title}</div>
+                            <small className="text-secondary">{session.description}</small>
+                          </td>
+                          <td>
+                            <div className="text-secondary">
+                              {dateTimeFormat(session.start_datetime)}
+                            </div>
+                            <div className="text-secondary">
+                              {dateTimeFormat(session.end_datetime)}
+                            </div>
+                          </td>
+                          <td>
+                            {disablePreviousDate(session.end_datetime) ? (
+                              <Badge className="bg-info">Not yet started!</Badge>
+                            ) : (
+                              <Badge className="bg-warning">Completed!</Badge>
+                            )}
+                          </td>
+                          <td>
+                            {session.link ? (
+                              <a href={session.link} target="_blank">
+                                {session.link}
+                              </a>
+                            ) : (
+                              <Badge className="bg-warning">Not Available</Badge>
+                            )}
+                          </td>
+                        </tr>
+                      ))}
+                  </tbody>
+                </Table>
+              </Container>
+            )}
+          </>
+        ) : (
+          <div className="text-secondary text-center">Please select start date and end date</div>
+        )}
       </StudentLayout>
     </div>
   );
